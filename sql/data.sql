e0c5e58e7ce53f2e235e2aecbcc2a29f9aabd48f
Peter Mellquist <pemellquist@gmail.com>
2013-06-18 05:08:25 +0000

    new

---
+++ sql/data.sql
@@ -0,0 +1,16 @@
+# SkyPunch Database schema
+# pemellquist@gmail.com
+
+USE skypunch;
+
+INSERT INTO targets
+VALUES (0,'Google home page','0','NEW',NULL,'NEW','','http://www.google.com','GET','NONE','',200,10,10,0,0,0,0,0,0,0,0,1);
+
+INSERT INTO targets
+VALUES (0,'HP home page ','0','NEW',NULL,'','NEW','http://www.hp.com','GET','NONE','',200,10,10,0,0,0,0,0,0,0,0,1);
+
+INSERT INTO targets
+VALUES (0,'Openstack Service','0','NEW',NULL,'','NEW','https://openstack.service.url','GET','OPENSTACK','user=xxxx,password=yyyyy,osauthendpoint=OPENSTACK_KEYSTONE_ENDPOINT,tenantid=8ZZZZZZZZZZ',200,10,10,0,0,0,0,0,0,0,0,1);
+
+INSERT INTO notifiers
+VALUES(0, 'Peters gmail','SMTP','pemellquist@gmail.com','user=XXXXXXX,password=YYYYYYYY,server=smtp.gmail.com');
