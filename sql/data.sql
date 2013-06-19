97c6697fdb35cf07c11faa19fba78af254783036
Peter Mellquist <pemellquist@gmail.com>
2013-06-19 17:26:10 +0000

    update

--- sql/data.sql
+++ sql/data.sql
@@ -7,10 +7,7 @@ INSERT INTO targets
 VALUES (0,'Google home page','0','NEW',NULL,'NEW','','http://www.google.com','GET','NONE','',200,10,10,0,0,0,0,0,0,0,0,1);
 
 INSERT INTO targets
-VALUES (0,'HP home page ','0','NEW',NULL,'','NEW','http://www.hp.com','GET','NONE','',200,10,10,0,0,0,0,0,0,0,0,1);
-
-INSERT INTO targets
-VALUES (0,'Openstack Service','0','NEW',NULL,'','NEW','https://openstack.service.url','GET','OPENSTACK','user=xxxx,password=yyyyy,osauthendpoint=OPENSTACK_KEYSTONE_ENDPOINT,tenantid=8ZZZZZZZZZZ',200,10,10,0,0,0,0,0,0,0,0,1);
+VALUES (0,'Openstack Service','0','NEW',NULL,'','NEW','https://openstack.service.url','GET','OPENSTACK','user=xxxx,password=yyyyy,osauthendpoint=OPENSTACK_KEYSTONE_ENDPOINT,tenantid=ZZZZZZZZZZ',200,10,10,0,0,0,0,0,0,0,0,1);
 
 INSERT INTO notifiers
 VALUES(0, 'Peters gmail','SMTP','pemellquist@gmail.com','user=XXXXXXX,password=YYYYYYYY,server=smtp.gmail.com');
