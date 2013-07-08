7d890e20c5ffe10ec0b967752a27662c9ca8256f
Peter Mellquist <pemellquist@gmail.com>
2013-07-08 18:47:16 +0000

    refactor

--- skypunch/puncher.py
+++ skypunch/puncher.py
@@ -19,13 +19,16 @@ __docstring__ = """
 Puncher class performs the communication to the defined target,handles various authentication, records results
 and sends notifications.
 """
-
 from 	skypunchconfig import SkyPunchConfig
 from    skypunchexceptions import * 
 from 	targetmodel import TargetModel
+from    targetmodel import update_target_counters
+from    targetmodel import update_target_status
 from    notifiermodel import NotifierModel
 from    skypunchnotifier import SkyPunchNotifier
 from 	sqlalchemy import *
+from    basicauth import *
+from    openstackauth import get_openstack_auth  
 import 	time
 import 	datetime
 from 	urlparse import urlparse 
@@ -35,10 +38,6 @@ import  json
 import  sys
 import  socket
 
-USER = 'user'
-PASSWORD = 'password'
-OSAUTHENDPOINT = 'osauthendpoint'
-TENANTID = 'tenantid'
 TIMEOUT = 'timeout'
 PROTOCOL_HTTP = 'http'
 PROTOCOL_HTTPS = 'https'
@@ -53,73 +52,28 @@ class Puncher:
         self.logger = logger
         self.targetmodel = targetmodel
 
-    # compute http basic auth value
-    def get_basic_auth(self, target):
-        user = None
-        password = None	
-        authnargs = target.authn_parms
-        options = authnargs.split(',')
-        for option in options:
-            kv=option.split('=')
-            if kv[0] == USER:
-                user = kv[1]
-            if kv[0] == PASSWORD:
-                password = kv[1]
-        if user == None or password == None:
-            raise SkyPunchAuthParamError('user or password not defined')	
-        return base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
+    # process error condition and message
+    def process_error(self,target,message):
+        self.logger.error(' [%d] %-30s %s' %(target.id,target.name,message))
+        target.status = STATUS_FAIL
+        target.status_description = message
+        target.fail_count += 1
+        target.repeated_fails += 1
+        self.targetmodel.commit()
+        return
 
-    # get Openstack token from Keystone auth endpoint
-    def get_openstack_auth(self,target):
-        user = None
-        password = None
-        os_auth_endpoint = None
-        tenantid = None
-        authnargs = target.authn_parms
-        options = authnargs.split(',')
-        for option in options:
-            kv=option.split('=')
-            if kv[0] == USER:
-                user = kv[1]
-            if kv[0] == PASSWORD:
-                password = kv[1]
-            if kv[0] == OSAUTHENDPOINT:
-                os_auth_endpoint = kv[1]
-            if kv[0] == TENANTID :
-                tenantid = kv[1]
-        if user == None or password == None or os_auth_endpoint == None or tenantid == None:
-            raise SkyPunchAuthParamError('user or password or osauthendpoint or tenantid not defined')
-        try:        
-            url = urlparse(os_auth_endpoint)
-        except:
-            self.logger.warn('target protocol %s not supported for %s' % (url.scheme, os_auth_endpoint)) 
-            raise SkyPunchInvalidProtocolError('target protocol %s not supported for %s' % (url.scheme, os_auth_endpoint)) 
-        location = url.netloc
-        locations = location.split(':')
-        kwargs = {TIMEOUT: target.timeout}
-        if url.scheme == PROTOCOL_HTTP:
-            conn = httplib.HTTPConnection(locations[0],url.port,**kwargs)
-        elif url.scheme == PROTOCOL_HTTPS:
-            conn = httplib.HTTPSConnection(locations[0], url.port,**kwargs)
+    # log the result line
+    def log_result(self,target):
+        message = ' [%d] %-30s %s %s %s (%s)' % (target.id, target.name, target.method, target.url, target.status, target.status_description)
+        if target.status == STATUS_PASS:
+            self.logger.info(' %s' % message)
         else:
-            raise SkyPunchInvalidProtocolError('target protocol not supported')
-        conn.connect()
-        request = conn.putrequest('POST',url.path)
-        login = '{ \"auth\":{ \"passwordCredentials\": { \"username":\"%s\",\"password\":\"%s\" }, \"tenantId\":\"%s\" } }' % (user,password,tenantid)
-        conn.putheader("Content-Length",len(login))
-        conn.putheader("Content-Type","application/json")	
-        conn.endheaders()
-        conn.send(login)
-        response = conn.getresponse()
-        if response.status != 200:
-             raise SkyPunchKeystoneAuthError('Openstack keystone auth failure %d %s' % (response.status,response.reason)) 
-        data = response.read()
-        json_login = json.loads(data)
-        keystone_token = json_login['access']['token']['id']	
-        return keystone_token 
+            self.logger.error(message)
 
     # punch a target right now
     def punch_it_now(self,target,notifiermodel):
+        response = None
+        failed = False
         try:
             # parse out the target URL
             url = urlparse(target.url)
@@ -141,114 +95,64 @@ class Puncher:
             # HTTP Basic authentication  
             if target.authn == 'BASIC':
                 try:
-                    auth = self.get_basic_auth(target)
+                    auth = get_basic_auth(target)
                     conn.putheader("Authorization", "Basic %s" % auth) 
                 except SkyPunchAuthParamError as spe:
-                    self.logger.error('[%d] %-30s invalid BASIC authn params (user and password required)' % (target.id, target.name))
-                    target.status = STATUS_FAIL
-                    target.status_description = 'invalid BASIC authn params (user and password required)'
-                    target.fail_count += 1
-                    target.repeated_fails += 1
-                    self.targetmodel.commit()
+                    # improperly defined target, log error but do not notify
+                    self.process_error(target,'invalid BASIC authn params (user and password required)')                
                     return
+            
             # Openstack Keystone token based authentication
             elif target.authn == 'OPENSTACK':
                 try:
-                    auth = self.get_openstack_auth(target)
+                    auth = get_openstack_auth(target)
                     conn.putheader("X-Auth-Token",auth)
                 except SkyPunchAuthParamError:
-                    self.logger.error('[%d] %-30s invalid Openstack Keystone authn params (user,password,tenantid,osauthendpoint)' % (target.id, target.name))
-                    target.status = STATUS_FAIL
-                    target.status_description = 'invalid Openstack / Keystone authn params (user,password,tenantid,osauthendpoint)'
-                    target.fail_count += 1
-                    target.repeated_fails += 1
-                    self.targetmodel.commit()
+                    # improperly defined target params, log error but do not notify
+                    self.process_error(target,'invalid Openstack / Keystone authn params (user,password,tenantid,osauthendpoint)')
                     return
                 except SkyPunchInvalidProtocolError:
-                    self.logger.error('[%d] %-30s invalid Openstack Keystone endpoint' % (target.id, target.name))
-                    target.status = STATUS_FAIL
-                    target.status_description = 'invalid Openstack / Keystone endpoint protocol)'
-                    target.fail_count += 1
-                    target.repeated_fails += 1
-                    self.targetmodel.commit()
+                    # improperly defined target params, log error but do not notify
+                    self.process_error(target,'invalid Openstack / Keystone endpoint protocol)')
                     return
                 except SkyPunchKeystoneAuthError as spe:
-                    self.logger.error('[%d] %-30s %s' % (target.id, target.name, spe))
-                    target.status = STATUS_FAIL
-                    target.status_description = 'Openstack Keystone authn failure' 
-                    target.fail_count += 1
-                    target.repeated_fails += 1
-                    self.targetmodel.commit()
-                    return
+                    update_target_status(target,STATUS_FAIL, str(spe)) 
+                    failed = True
 
-            # add headers and issue request
-            conn.endheaders()
-            conn.send('')
-            response = conn.getresponse()
+            if not failed:
+                # add headers and issue request
+                conn.endheaders()
+                conn.send('')
+                response = conn.getresponse()
 
-            # check response, update counters and commit to db 
-            if response.status == target.pass_result:
-                target.previous_status = target.status
-                target.status = STATUS_PASS 
-                target.status_description = response.reason 
-                target.pass_count += 1
-                target.repeated_fails = 0
-            else:
-                target.previous_status = target.status
-                target.status = STATUS_FAIL 
-                target.status_description = 'target status:%d != %d' % ( response.status, target.pass_result)
-                target.fail_count += 1
-                target.repeated_fails += 1
-            httpStatus = response.status
-        except socket.error:
-            target.previous_status = target.status
-            target.status = STATUS_FAIL 
-            target.fail_count +=1
-            target.repeated_fails += 1
-            target.status_description = 'network error to target' 
-            httpStatus = 600
+                update_target_status(target,
+                    STATUS_PASS if response.status == target.pass_result else STATUS_FAIL,
+                    response.reason if response.status == target.pass_result else ('target status:%d != %d' % (response.status,target.pass_result))) 
+
+        except socket.error as se:
+            update_target_status(target,STATUS_FAIL,str(se))
         except:
-            target.previous_status = target.status
-            target.status = STATUS_FAIL
-            target.fail_count +=1
-            target.repeated_fails += 1
-            target.status_description = 'unable to read target response' 
-            httpStatus = 600
-        message = '[%d] %-30s %s %s %s (%s)' % (target.id, target.name, target.method, target.url, target.status, target.status_description)
-        
-        if target.status == STATUS_PASS:
-            self.logger.info(' %s' % message)
-        else:
-            self.logger.error(message)
+            update_target_status(target,STATUS_FAIL,sys.exc_info()[0])
+
+        self.log_result(target)
 
         # notify 
         notifier = SkyPunchNotifier(self.logger)
         notifier.notify(target,notifiermodel)
         
         # update counters
-        self.update_counters(target,httpStatus)
+        update_target_counters(target,response)
         
         # save back to db
         self.targetmodel.commit()
 
-    # update target counters
-    def update_counters(self,target,status):
-	if status >=200 and status < 300:
-            target.count200 += 1
-        elif status >= 300 and status < 400:
-            target.count300 += 1
-        elif status >= 400 and status < 500:
-            target.count400 += 1
-        elif status >= 500 and status < 600:
-            target.count500 += 1
-        else:
-            target.network_fails += 1
-
     # find a target to punch
     def punch(self,target,notifiermodel):
+        # if it is new, test it right away
         if target.status == STATUS_NEW:
             self.punch_it_now(target,notifiermodel)
+        # check if its time to test
         else:
             delta = datetime.datetime.now() - target.last_updated
-            if delta.seconds>target.frequency:
+            if delta.seconds > target.frequency:
                 self.punch_it_now(target,notifiermodel)
